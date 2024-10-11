0b07f84b26e7c05d6e4ca741445b871baaa42a86
nichnikov <a.nichnikov@google.com>
2024-10-11 08:27:56 +0000

    ref

---
+++ mistral_nemo_download.py
@@ -0,0 +1,8 @@
+import os
+from huggingface_hub import snapshot_download
+from pathlib import Path
+
+mistral_models_path = os.path.join(os.getcwd(), "models", 'mistral_models', 'Nemo-Instruct')
+# mistral_models_path.mkdir(parents=True, exist_ok=True)
+
+snapshot_download(repo_id="mistralai/Mistral-Nemo-Instruct-2407", allow_patterns=["params.json", "consolidated.safetensors", "tekken.json"], local_dir=mistral_models_path)
